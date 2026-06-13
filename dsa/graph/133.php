c53c23d3346757465726a941a8434002b2fb4f8b
Leandro Ventura <leandroventur19@gmail.com>
2026-06-13 13:33:24 -0300

    graph

---
+++ dsa/graph/133.php
@@ -0,0 +1,24 @@
+<?php
+
+class Node {
+    public $val = null;
+    public $neighbors = null;
+    function __construct($val = 0) {
+        $this->val = $val;
+        $this->neighbors = array();
+    }
+}
+
+class Solution {
+    /**
+     * @param Node $node
+     * @return Node
+     */
+    function cloneGraph($node) {
+        $newNode = ;
+        $visited = [];
+        
+        
+    }
+
+}
